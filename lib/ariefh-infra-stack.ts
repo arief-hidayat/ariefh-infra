a714b51949a43da1f978928410f60488db3b2508
Arief Hidayat <mr.arief.hidayat@gmail.com>
2022-12-09 04:23:07 +0700

    feat: dev-vpc on 2 AZs with public, private, and isolated subnets

--- lib/ariefh-infra-stack.ts
+++ lib/ariefh-infra-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AriefhInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,5 +13,33 @@ export class AriefhInfraStack extends Stack {
     // const queue = new sqs.Queue(this, 'AriefhInfraQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const vpc = new ec2.Vpc(this, 'dev-vpc', {
+      cidr: '10.0.0.0/16',
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+          cidrMask: 24,
+        },
+        {
+          name: 'isolated',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 28,
+        },
+      ],
+    });
+
+
+    new cdk.CfnOutput(this, 'vpcId', {
+      value: vpc.vpcId,
+      description: 'the ID of the VPC',
+    });
   }
 }
